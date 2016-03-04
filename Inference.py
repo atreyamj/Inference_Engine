a581ed03e010bcf5fa23cc463a91c896568f5be0
atreyamj <atreyamj@usc.edu>
2016-03-03 20:38:50 -0800

    Term Dict Building
    Term Dict Building Phase 0

--- Inference.py
+++ Inference.py
@@ -1,5 +1,5 @@
-#RHSDict(RHS,String[][]LHS)
-#TermDict(With_Sign_Term,String[] Params) getParams, setParams
+#RHSDict(RHS,String[]LHS) - Traitor, LHS[0]-
+#TermDict(Term,String[] Params) getParams, setParams
 #FindTerms(Query) - True or False
 
 class ParseInference:
@@ -7,20 +7,21 @@ class ParseInference:
     RHSDict=dict()
     TermDict=dict()
     OperatorDict=dict()
-    OperatorDict[0]="||"
-    OperatorDict[1]="&&"
-    OperatorDict[2]="=>"
+    OperatorDict[0]="&&"
+    OperatorDict[1]="=>"
     def AddtoKB(self,fact):
         Term=""
         Facts=fact.split("&&")
         for eachfact in Facts:
-           # print eachfact.replace(" ","")
-            print eachfact.replace(" ","").split("(",1)[0]
-
-
+            term=eachfact.replace(" ","").split("(")[0]
+            params=eachfact.replace(" ","").split("(",1)[1]
+            paramsArray= params.replace(")","").split(",")
+            self.TermDict[term]=paramsArray
 
 
 Obj=ParseInference()
-Obj.AddtoKB("ViterbiSquirrel(x) && Secret(y)&&Tells(x,y,z)&&Hostile(z)")
+Obj.AddtoKB("ViterbiSquirrel(x) && Secret(y) && Tells(x,y,z) && Hostile(z)")
+print Obj.TermDict["Tells"]
+
 
 
